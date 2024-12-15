87c93238c7dd192049ff048fe78b6f991d9e9b7d
ayomenjadi <ayomenjadi@gmail.com>
2024-12-15 05:28:28 +0000

    feat(product): Add product category and gender

--- product/models/setup.go
+++ product/models/setup.go
@@ -13,30 +13,25 @@ var DB *gorm.DB
 
 func ConnectDatabase() {
 
-	// Load environment variables from the .env file
 	if err := godotenv.Load(); err != nil {
 		log.Fatalf("Failed to load .env file: %v", err)
 	}
 
-	// Construct the DSN (Data Source Name) for MySQL connection
 	dsn := os.Getenv("DB_USER") + ":" +
 		os.Getenv("DB_PASS") + "@tcp(" +
 		os.Getenv("DB_HOST") + ":" +
 		os.Getenv("DB_PORT") + ")/" +
 		os.Getenv("DB_NAME") + "?charset=utf8mb4&parseTime=True&loc=Local"
 
-	// Open the database connection
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Failed to connect to the database: %v", err)
 	}
 
-	// Perform database migration
-	if err := db.AutoMigrate(&TypeSale{}); err != nil {
+	if err := db.AutoMigrate(&TypeSale{}, &Cat{}, &Gen{}); err != nil {
 		log.Fatalf("Failed to migrate the database: %v", err)
 	}
 
-	// Assign the connection to the global variable
 	DB = db
 	log.Println("Successfully connected to the database.")
 }
