bbde3cfb4b9d279b6d2c505a85ca71b062966857
ayomenjadi <ayomenjadi@gmail.com>
2024-12-27 12:01:04 +0000

    fix(auth): Fix data cant update

--- auth/models/user.go
+++ auth/models/user.go
@@ -3,17 +3,17 @@ package models
 import "time"
 
 type User struct {
-	Username  string    `gorm:"type:varchar(100);primaryKey;" json:"username"`
-	Password  string    `gorm:"type:varchar(255);" json:"password"`
-	Email     string    `gorm:"type:varchar(100);unique" json:"email"`
-	FirstName string    `gorm:"type:varchar(255);null" json:"first_name"`
-	LastName  string    `gorm:"type:varchar(255);null" json:"last_name"`
-	Image     string    `gorm:"type:varchar(255);null" json:"image"`
-	Desc      string    `gorm:"type:varchar(255);null" json:"desc"`
-	Hp        string    `gorm:"type:varchar(20);null" json:"hp"`
-	Type      int       `gorm:"type:int(11)" json:"type"`
-	TypeInfo  TypeUser  `gorm:"foreignKey:Type;references:ID" json:"type_info"`
-	CreatedAt time.Time `json:"created_at"`
+    Username  string    `gorm:"type:varchar(100);primaryKey;" json:"username"`
+    Password  string    `gorm:"type:varchar(255);" json:"password"`
+    Email     string    `gorm:"type:varchar(100);unique" json:"email"`
+    FirstName string    `gorm:"column:first_name;type:varchar(255);null" json:"first_name"`
+    LastName  string    `gorm:"column:last_name;type:varchar(255);null" json:"last_name"`
+    Image     string    `gorm:"type:varchar(255);null" json:"image"`
+    Desc      string    `gorm:"type:varchar(255);null" json:"desc"`
+    Hp        string    `gorm:"type:varchar(20);null" json:"hp"`
+    Type      int       `gorm:"type:int(11)" json:"type"`
+    TypeInfo  TypeUser  `gorm:"foreignKey:Type;references:ID" json:"type_info"`
+    CreatedAt time.Time `json:"created_at"`
 }
 
 func (User) TableName() string {
