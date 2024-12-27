c0186037bd811f59850a6446c419a3485d132fc8
ayomenjadi <ayomenjadi@gmail.com>
2024-12-27 11:48:55 +0000

    fix(auth): Fix data cant update

--- auth/models/user.go
+++ auth/models/user.go
@@ -5,9 +5,9 @@ import "time"
 type User struct {
 	Username  string    `gorm:"type:varchar(100);primaryKey;" json:"username"`
 	Password  string    `gorm:"type:varchar(255);" json:"password"`
-	FirstName string    `gorm:"type:varchar(255);" json:"first_name"`
-	LastName  string    `gorm:"type:varchar(255);" json:"last_name"`
 	Email     string    `gorm:"type:varchar(100);unique" json:"email"`
+	FirstName string    `gorm:"type:varchar(255);null" json:"first_name"`
+	LastName  string    `gorm:"type:varchar(255);null" json:"last_name"`
 	Image     string    `gorm:"type:varchar(255);null" json:"image"`
 	Desc      string    `gorm:"type:varchar(255);null" json:"desc"`
 	Hp        string    `gorm:"type:varchar(20);null" json:"hp"`
